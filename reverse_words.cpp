7e876020c16d32228e0510cb12db7db641343741
MD Akram <115627463+IamAkram321@users.noreply.github.com>
2026-02-13 19:02:27 +0000

    add solution for reverse words

---
+++ reverse_words.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> reverse(string s){
+    int n = s.size();
+    reverse(s.begin(),s.end());
+    string ans = "";
+    for(int i=0;i<n;i++){
+        string word="";
+        while(i<n && s[i]!=' '){
+            word+=s[i];
+            i++;
+        }
+        reverse(word.begin(),word.end());
+        if(word.size()>0){
+            ans += " "+word;
+        }
+    }
+    return ans.substr(1);
+}
+
+int main(){
+     string str = "the sky is blue";
+     string ans = reverse(str);
+     cout<<ans;
+}
+
+/**************************USING RECURSION**************************************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string reverseWordsRec(string s){
+    int n = s.size();
+    int i=0;
+    while(i<n && s[i]==' '){ //removing leading spaces
+        i++;
+    }
+    s = s.substr(i);
+    while(!s.empty() && s.back()==' '){ //removing trainling spaces
+        s.pop_back();
+    }
+    if(s.size()==0) return ""; //base case
+    int spacePos = s.find(' ');
+    if(spacePos == string::npos) return s; //if there is only one word
+    string first_word = s.substr(0,spacePos);
+    string remaining = s.substr(spacePos+1);
+    string rec_ans=reverseWordsRec(remaining);
+    return rec_ans+" "+first_word;
+}
+
+int main(){
+     string str = "the sky is blue";
+     string ans = reverseWordsRec(str);
+     cout<<ans;
+}
